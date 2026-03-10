a4709547491444fb66cb601dff8bdafecb24a383
Hijin554 <dhgmlwls86@gmil.com>
2026-03-10 09:44:57 +0900

    승표씨

--- src/models/recommendModel.js
+++ src/models/recommendModel.js
@@ -11,7 +11,15 @@ export const getRandomFoods = async (limit = 5) => {
         food_name AS name, 
         -- image_url이 foods 테이블에 없으면 null을 반환합니다. 
         -- 만약 있다면 column 이름을 맞추어 수정하세요.
-        null AS image 
+        null AS image ,
+        calories AS kcal,
+        carbohydrate AS carbs,
+        protein,
+        fat,
+        sugars AS sugar,
+        null AS status
+        
+        
     FROM foods 
     ORDER BY RANDOM() 
     LIMIT $1
